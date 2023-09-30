b901687d2e7034a7760bf18015ed9d8a2088b2d7
Bennett Biju Mathew <tekkit25637@gmail.com>
2023-09-30 19:13:12 +1000

    Done.

--- source/script.js
+++ source/script.js
@@ -23,36 +23,95 @@ class Task {
     }
 }
 
-tasks = [];
-
-firstTask = new Task("Finish Homework", "24/03/13", "Science", "School");
-tasks[0] = firstTask;
-
-firstTask = new Task("Finish Cooking", "28/04/13", "Create Biriyani", "Hoome");
-tasks[1] = firstTask;
-
-console.log(tasks[1].getName());
-
-function displayTask() {
-    html = "";
-
-    for (i = 0; i < tasks.length; i++) {
-        html += `<aside class="task"> 
-                    <div> 
-                        <h3> ${tasks[i].getName()} </h3>
-                        <p> ${tasks[i].getDate()} | ${tasks[i].getDescription()} | ${tasks[i].getPlanner()} </p>    
-                    </div>
-
-                    <input type="button" class="task-button button">
-                </aside>`;
+class Planner {
+    constructor (name, colour) {
+        this.name = name;
+        this.colour = colour; 
     }
 
-    document.getElementById("task-list").innerHTML = html;
+    getName() {
+        return this.name;
+    }
+    
+    getColour() {
+        return this.colour;
+    }
+}
+
+tasks = [];
+planners = [];
+
+firstPlanner = new Planner("School", "#7EA16B");
+planners[0] = firstPlanner;
+firstTask = new Task("Finish Homework", "24/03/13", "Science", firstPlanner);
+tasks[0] = firstTask;
+
+secondPlanner = new Planner("Testing", "#DECE85");
+planners[1] = secondPlanner;
+secondTask = new Task("Finish Cooking", "28/04/13", "Create Biriyani", secondPlanner);
+tasks[1] = secondTask;
+tasks[2] = secondTask;
+tasks[3] = secondTask;
+
+function displayTask(i) {
+    html = "";
+
+    html += `<aside class="task"> <div> <h3> ${tasks[i].getName()} </h3>  <span>`;
+
+    if (tasks[i].getPlanner().getName() != null) {
+        html += `<div class="task-planner-colour" style="background-color: ${tasks[i].getPlanner().getColour()}"> </div>`;
+        html += `${tasks[i].getPlanner().getName()}, `;
+    }
+
+    if (tasks[i].getDate() != null) {
+        html += `${tasks[i].getDate()}, `;
+    }
+    if (tasks[i].getDescription() != null) {
+        html += `${tasks[i].getDescription()}, `;
+    }
+
+    html += `</span> </div> <input type="button" class="task-button button"> </aside>`;
+    
+    return html;
 }
 
 function addTask() {
-    details = document.getElementById("task-input").value;
-    taskDetails = details.split(', ');
-    tasks[tasks.length+1] = new Task(taskDetails[0], taskDetails[1], taskDetails[2], taskDetails[3]);
-    displayTask();
+    if (event.key == 'Enter' && document.getElementById("task-input").value != "") {
+        details = document.getElementById("task-input").value;
+        taskDetails = details.split(', ');
+        plannerDetails = new Planner(taskDetails[3], "#54333A");
+        tasks.push(new Task(taskDetails[0], taskDetails[1], taskDetails[2], plannerDetails));
+        displayTasks();
+    }
+}
+
+function displayPlanners() {
+    html = "";
+
+    for (i = 0; i < planners.length; i++) {
+        html += 
+        `<aside class="planner">
+            <input type="checkbox" name="${planners[i].getName()}" class="planner-checkbox checkbox" style="accent-color: ${planners[i].getColour()}" onclick="displayTasks()" checked> 
+            <p class="planner-text"> ${planners[i].getName()} </p>
+        </aside>`
+    }
+
+    document.getElementById("planner-list").innerHTML = html;
+}
+
+function displayTasks() {
+    const inputFields = document.querySelectorAll("#planner-list input[type='checkbox']");
+    html = "";
+
+    for (i = 0; i < inputFields.length; i++) {
+        if (inputFields[i].checked == true) {
+            for (j = 0; j < tasks.length; j++) {
+                if (tasks[j].getPlanner().getName() == planners[i].getName()) {
+                    html += displayTask(j);
+                }
+            }  
+        }
+    }
+
+    document.getElementById("task-list").innerHTML = html;
 }
